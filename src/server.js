0426914e15f73b032bc62733e4b01b488b08b9a2
souleymane kone <desolo74@gmail.com>
2021-04-04 09:14:30 -0700

    all

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
  const app = express();
- const store = require('./store.json')
+ const store = require('../store.json')
 
 //  const PORT = process.env.PORT || 3000;
 
