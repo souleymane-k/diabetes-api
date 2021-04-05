4aeb1be1da2e827d2b1d0f5c998c6c753e197521
souleymane kone <desolo74@gmail.com>
2021-04-04 20:12:52 -0700

    all

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
  const app = express();
  const STORE = require('../store.json')
+ const { PORT } = require('./config')
 
 //  const PORT = process.env.PORT || 3000;
 
@@ -22,7 +23,7 @@ app.get('/results', (req, res)=>{
 
 app.get('/months/:month_id', (req, res)=>{
   const{month_id}=req.parasms;
-  const month = STORE.months.map(m => m.id ==month_id);
+  const month = STORE.months.find(m => m.id ==month_id);
     if(!month){
       return res.send('Month Required')
     }
@@ -31,7 +32,7 @@ app.get('/months/:month_id', (req, res)=>{
 
 app.get('/results/:result_id', (req, res)=>{
   const{result_id}=req.parasms;
-  const result = STORE.results.map(r => r.id ==result_id);
+  const result = STORE.results.find(r => r.id ==result_id);
     if(!result){
       return res.send('result Required')
     }
@@ -40,34 +41,32 @@ app.get('/results/:result_id', (req, res)=>{
 
 
 
-app.get('/months', function getMonths(req, res) {
-  let response = STORE.months;
 
-  // filter our pokemon by name if name query param is present
-  if (req.query.name) {
-    response = response.filter(months =>
-      // case insensitive searching
-      months.name.toLowerCase().includes(req.query.name.toLowerCase())
-    )
-  }
-
-  //filter our pokemon by type if type query param is present
-  if (req.query.types) {
-    response = response.filter(pokemon =>
-      pokemon.type.includes(req.query.types)
-      //req.query.types
-    )
-  }
-
-  res.json(response)
-})
-
-
-
-// const app = require('./app')
-const { PORT } = require('./config')
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
 module.exports = {app};
+
+
+
+
+
+// app.get('/months', function getMonths(req, res) {
+//   let response = STORE.months;
+
+//   // filter our pokemon by name if name query param is present
+//   if (req.query.name) {
+//     response = response.filter(months =>
+//       // case insensitive searching
+//       months.name.toLowerCase().includes(req.query.name.toLowerCase())
+//     )
+//   }
+
+//   //filter our pokemon by type if type query param is present
+//   if (req.query.types) {
+//     response = response.filter(pokemon =>
+//       pokemon.type.includes(req.query.types)
+//       //req.query.types
+//     )
+//   }
