65959224ea7c79756f8fedbda001e7c11b45bc1c
souleymane kone <desolo74@gmail.com>
2021-04-07 13:56:35 -0700

    working on post and delete endpoints

--- src/server.js
+++ src/server.js
@@ -123,10 +123,10 @@ app.delete('/results/:result_id', (req, res)=>{
  
   STORE.results.splice(resultIndex, 1)
   
-  // logger.info(`result with id ${result_id} deleted.`)
-  //   res
-  //     .status(204)
-  //     .end()
+  logger.info(`result with id ${result_id} deleted.`)
+    res
+      .status(204)
+      .end()
 
 })
 
