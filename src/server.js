d5d12fe9819eba322156a81de735401648a459bc
souleymane kone <desolo74@gmail.com>
2021-04-07 08:00:52 -0700

    id

--- src/server.js
+++ src/server.js
@@ -139,15 +139,15 @@ if (!dtype) {
   logger.error(`Invalid dtype '${dtype}' is required`)
   return res.status(400).send(`'dtype' is required`)
 }
-const result = { id: uuid(), monthName, mealName, result, date,monthId,description,dtype }
+const input = { id: uuid(), monthName, mealName, result, date,monthId,description,dtype }
 
-STORE.results.push(result)
+STORE.results.push(input)
 
 logger.info(`Result with id ${result.id} created`)
     res
       .status(201)
       .location(`http://localhost:8001/results/${result.id}`)
-      .json(result)
+      .json(input)
 
 
 })
