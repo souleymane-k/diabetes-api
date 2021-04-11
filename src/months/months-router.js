711bba121bc672c039c1011f1c3a1468359d3a15
souleymane kone <desolo74@gmail.com>
2021-04-11 11:55:00 -0700

    add router

--- src/months/months-router.js
+++ src/months/months-router.js
@@ -1,5 +1,6 @@
 //const path = require('path')
 const express = require('express')
+const STORE = require('../store.json')
 
 const monthsRouter = express.Router()
 // const bodyParser = express.json()
