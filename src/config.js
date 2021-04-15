824cc3138577f8c8b8638c4bee382c3709847d62
souleymane kone <desolo74@gmail.com>
2021-04-15 13:25:37 -0700

    add api

--- src/config.js
+++ src/config.js
@@ -5,5 +5,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'production',
     CLIENT_ORIGIN:'https://diabetes-ms-3skoas3bd-souleymane-k.vercel.app/',
     API_TOKEN: process.env.API_TOKEN || '0d0e1dbb-8d52-49f0-9dbf-570ec4f5109a',
+    DB_URL="postgresql://dunder_mifflin:hello@localhost/diabetes"
   }
 
--- src/server.js
+++ src/server.js
@@ -51,6 +51,6 @@ app.use((error, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
-// module.exports = {app};
+
 
 
