66e95478963a3ee3fbd74b4087b7dc8722866488
souleymane kone <desolo74@gmail.com>
2021-04-15 16:18:45 -0700

    addons

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
     PORT: process.env.PORT || 8001,
     NODE_ENV: process.env.NODE_ENV || 'production',
-    CLIENT_ORIGIN:'https://diabetes-ms-3skoas3bd-souleymane-k.vercel.app/',
+    CLIENT_ORIGIN:'https://diabetes-ms-3skoas3bd-souleymane-k.vercel.app',
     API_TOKEN: process.env.API_TOKEN || '0d0e1dbb-8d52-49f0-9dbf-570ec4f5109a',
     DATABASE_URL:"postgresql://dunder_mifflin:hello@localhost/diabetes",
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://dunder_mifflin:hello@localhost/diabetes-test"
