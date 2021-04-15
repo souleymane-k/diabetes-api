cefafbcfb800cc44ad006f31a3c0c8b36c24e754
souleymane kone <desolo74@gmail.com>
2021-04-15 15:20:31 -0700

    add api

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'production',
     CLIENT_ORIGIN:'https://diabetes-ms-3skoas3bd-souleymane-k.vercel.app/',
     API_TOKEN: process.env.API_TOKEN || '0d0e1dbb-8d52-49f0-9dbf-570ec4f5109a',
-    DB_URL:"postgresql://dunder_mifflin:hello@localhost/diabetes"
+    DATABASE_URL:"postgresql://dunder_mifflin:hello@localhost/diabetes",
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://dunder_mifflin:hello@localhost/diabetes-test"
   }
 
