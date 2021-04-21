3b1efc76c0d11e4b0f2eb8daea12fd3738745086
souleymane kone <desolo74@gmail.com>
2021-04-21 13:05:56 -0700

    update main

--- src/app.js
+++ src/app.js
@@ -34,10 +34,10 @@ app.use(function errorHandler(error, req, res, next){
     let response
     if(NODE_ENV ==='production'){
     
-        response = { error: { message: error } }
+        response = {message: error.message.error}
     }else{
         console.error(error)
-        response = {message: error.message.error}
+         response = {message: error.message.error}
     }
     res.status(500).json(response)
 })
